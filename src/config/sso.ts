167ba58dca6bc88003c4cbf86ef200773894076a
Sachin Mandal <sachinmandal3580@gmail.com>
2026-07-19 15:56:45 +0530

    update

--- src/config/sso.ts
+++ src/config/sso.ts
@@ -1,5 +1,5 @@
 // erc_url =  https://sso.tech-iitb.org/project/7780c4ab-afcd-4b17-ab10-caf8e3abd0d7/ssocall/
 // local_url = https://sso.tech-iitb.org/project/883ee697-764c-4049-89d1-fc091b627d30/ssocall/ 
-// export const SSO_URL = `https://sso.tech-iitb.org/project/883ee697-764c-4049-89d1-fc091b627d30/ssocall/ ` //local_url
-export const SSO_URL = `https://sso.tech-iitb.org/project/7780c4ab-afcd-4b17-ab10-caf8e3abd0d7/ssocall/ ` //erc_url
+export const SSO_URL = `https://sso.tech-iitb.org/project/883ee697-764c-4049-89d1-fc091b627d30/ssocall/ ` //local_url
+// export const SSO_URL = `https://sso.tech-iitb.org/project/7780c4ab-afcd-4b17-ab10-caf8e3abd0d7/ssocall/ ` //erc_url
 export const SSO_USERDATA_URL = 'https://sso.tech-iitb.org/project/getuserdata'
