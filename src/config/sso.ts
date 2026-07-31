30d4e33fb32100714de4f39b5a1ca3ed6c41c6ac
Sachin Mandal <sachinmandal3580@gmail.com>
2026-07-31 19:30:52 +0530

    Switch SSO URLs to official versions

--- src/config/sso.ts
+++ src/config/sso.ts
@@ -1,9 +1,9 @@
 //SOR
-export const SOR_SSO_URL = `https://sso.tech-iitb.org/project/883ee697-764c-4049-89d1-fc091b627d30/ssocall/ ` //local_url
-//export const SOR_SSO_URL = `https://sso.tech-iitb.org/project/7780c4ab-afcd-4b17-ab10-caf8e3abd0d7/ssocall/ ` //official_url
+//export const SOR_SSO_URL = `https://sso.tech-iitb.org/project/883ee697-764c-4049-89d1-fc091b627d30/ssocall/ ` //local_url
+export const SOR_SSO_URL = `https://sso.tech-iitb.org/project/7780c4ab-afcd-4b17-ab10-caf8e3abd0d7/ssocall/ ` //official_url
 
 // XLR8
-export const XLR8_SSO_URL ="https://sso.tech-iitb.org/project/14058f66-7bac-4a81-a1ce-e440252f03f4/ssocall/"; //local_url
-//export const XLR8_SSO_URL ="https://sso.tech-iitb.org/project/7550c6cd-d62c-45f7-85a2-b5a1ee71489b/ssocall/"; //official_url
+//export const XLR8_SSO_URL ="https://sso.tech-iitb.org/project/14058f66-7bac-4a81-a1ce-e440252f03f4/ssocall/"; //local_url
+export const XLR8_SSO_URL ="https://sso.tech-iitb.org/project/7550c6cd-d62c-45f7-85a2-b5a1ee71489b/ssocall/"; //official_url
 
 export const SSO_USERDATA_URL = 'https://sso.tech-iitb.org/project/getuserdata'
