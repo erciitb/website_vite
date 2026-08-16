3dad1aff7f34ebd4805e11048011aafb70dfc619
Apla <131801034+RRoy4@users.noreply.github.com>
2026-08-16 21:43:04 +0530

    Update Xlr8registration.tsx

--- src/components/Xlr8registration.tsx
+++ src/components/Xlr8registration.tsx
@@ -2,6 +2,7 @@ import React, { useState, FormEvent, ChangeEvent } from 'react';
 import aditya from '../assets/aditya_qr.jpeg';
 import daksh from '../assets/WhatsApp Image 2026-08-16 at 1.13.08 PM.jpeg'
 import daksh_1 from '../assets/WhatsApp Image 2026-08-16 at 1.30.36 PM.jpeg'
+import qr from '../assets/IMG-20260816-WA0044(1).jpg'
 import {
   Users,
   User,
@@ -1220,7 +1221,7 @@ export default function XLR8Registration() {
                 </p>
 
                 <img
-                  src={daksh_1}
+                  src={qr}
                   alt="Scanner 1 - ₹1,800 Payment"
                   className="w-full max-w-xs rounded-xl object-contain"
                 />
