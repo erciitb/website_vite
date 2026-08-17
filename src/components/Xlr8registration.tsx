60ef6a92541b799d27839095792898b1fe7da73b
Sachin Mandal <sachinmandal3580@gmail.com>
2026-08-17 19:16:43 +0530

    Update Xlr8registration.tsx

--- src/components/Xlr8registration.tsx
+++ src/components/Xlr8registration.tsx
@@ -4,6 +4,7 @@ import daksh from '../assets/WhatsApp Image 2026-08-16 at 1.13.08 PM.jpeg'
 import daksh_1 from '../assets/WhatsApp Image 2026-08-16 at 1.30.36 PM.jpeg'
 import qr from '../assets/IMG-20260816-WA0044(1).jpg'
 import rr_qr from '../assets/RR_QR.png'
+import aditya_qr2 from '../assets/aditya_qr2.jpeg'
 import {
   Users,
   User,
@@ -1222,7 +1223,7 @@ export default function XLR8Registration() {
                 </p>
 
                 <img
-                  src={rr_qr}
+                  src={aditya_qr2}
                   alt="Scanner 1 - ₹1,800 Payment"
                   className="w-full max-w-xs rounded-xl object-contain"
                 />
